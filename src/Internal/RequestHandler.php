7779aa8d0b9cc1d89a38c87d8f08d8d2f37f469a
Jakub Groncki <jakobmats@dahmer.home>
2019-09-19 21:01:40 +0200

    Cleanup: container instance, added support for response transformers

--- src/Internal/RequestHandler.php
+++ src/Internal/RequestHandler.php
@@ -6,6 +6,7 @@ use Amp\Http\Server\Request;
 use Amp\Http\Server\RequestHandler as RequestHandlerInterface;
 use Amp\Http\Server\Router;
 use Amp\Promise;
+use Empress\ResponseTransformerInterface;
 use Psr\Container\ContainerInterface;
 
 use function Amp\call;
@@ -15,12 +16,16 @@ final class RequestHandler implements RequestHandlerInterface
     /** @var \Closure */
     private $closure;
 
-    /** @var Psr\Container\ContainerInterface */
+    /** @var Psr\Container\ContainerInterface|null */
     private $container;
 
-    public function __construct(\Closure $closure, ContainerInterface $container = null)
+    /** @var \Empress\ResponseTransformerInterface|null */
+    private $responseTransformer;
+
+    public function __construct(\Closure $closure, ResponseTransformerInterface $responseTransformer = null, ContainerInterface $container = null)
     {
         $this->closure = $closure;
+        $this->responseTransformer = $responseTransformer;
         $this->container = $container;
     }
 
@@ -30,11 +35,12 @@ final class RequestHandler implements RequestHandlerInterface
     public function handleRequest(Request $request): Promise
     {
         $params = $request->getAttribute(Router::class);
+        $promise = call($this->closure, $params, $request, $this->container);
 
-        if (is_null($this->container)) {
-            return call($this->closure, $params, $request);
+        if (!is_null($this->responseTransformer)) {
+            $promise = $this->responseTransformer->transform($promise);
         }
 
-        return call($this->closure, $params, $request, $this->container);
+        return $promise;
     }
 }
