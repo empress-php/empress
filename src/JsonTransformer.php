b9a844a794396751a07e6d4be9e5111c889bc263
Jakub Groncki <jakobmats@dahmer.home>
2019-09-19 21:02:11 +0200

    Added response transformer interface and JSON transformer class

---
+++ src/JsonTransformer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Empress;
+
+use Amp\Failure;
+use Amp\Http\Server\Response;
+use Amp\Success;
+use Amp\Promise;
+use function Amp\call;
+
+class JsonTransformer implements ResponseTransformerInterface
+{
+
+    /** @inheritDoc */
+    public function transform(\Amp\Promise $promise): Promise
+    {
+        return call(function () use ($promise) {
+
+            /** @var \Amp\Http\Server\Response $response */
+            $result = json_encode(yield $promise, JSON_THROW_ON_ERROR);
+
+            $response = new Response;
+            $response->setHeader('content-type', 'application/json');
+            $response->setBody($result);
+
+            return new Success($response);
+        });
+    }
+}
---
+++ src/ResponseTransformerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Empress;
+
+use Amp\Http\Server\Response;
+use Amp\Promise;
+
+interface ResponseTransformerInterface
+{
+
+    /**
+     * Takes a value and transforms it into a promise that will
+     * eventually resolve to a response
+     *
+     * @param \Amp\Promise<\Amp\Http\Server\Response> $promise
+     * @return \Amp\Promise<\Amp\Http\Server\Response>
+     */
+    public function transform(Promise $promise): Promise;
+}
