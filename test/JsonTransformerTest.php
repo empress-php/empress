6811f6948a5ef5cf345ad488ab22cab3c7098697
Jakub Groncki <jakobmats@dahmer.home>
2019-09-20 14:53:29 +0200

    Added JsonTransformer tests

---
+++ test/JsonTransformerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Empress\Test;
+
+use Amp\Success;
+use Amp\PHPUnit\AsyncTestCase;
+use Empress\JsonTransformer;
+
+class JsonTransformerTest extends AsyncTestCase
+{
+    public function testEncodeInf()
+    {
+        $this->expectException(\JsonException::class);
+
+        $responseTransformer = new JsonTransformer();
+        $infValue = new Success(\INF);
+
+        yield $responseTransformer->transform($infValue);
+    }
+
+    public function testEncodeArray()
+    {
+        $responseTransformer = new JsonTransformer();
+        $array = new Success(['status' => 'ok']);
+
+        /** @var \Amp\Http\Server\Response $transformed */
+        $transformed = yield $responseTransformer->transform($array);
+
+        $this->assertEquals(json_encode(['status' => 'ok']), yield $transformed->getBody()->read());
+    }
+}
