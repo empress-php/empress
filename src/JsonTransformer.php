ff2dc7e923e2552cd1b445f1db8e8d106c773b6c
Jakub Groncki <jakobmats@dahmer.home>
2019-09-20 13:38:41 +0200

    Fix: PHP version compat issue in JsonTransformer

--- src/JsonTransformer.php
+++ src/JsonTransformer.php
@@ -2,7 +2,6 @@
 
 namespace Empress;
 
-use Amp\Failure;
 use Amp\Http\Server\Response;
 use Amp\Success;
 use Amp\Promise;
@@ -16,8 +15,17 @@ class JsonTransformer implements ResponseTransformerInterface
     {
         return call(function () use ($promise) {
 
-            /** @var \Amp\Http\Server\Response $response */
-            $result = json_encode(yield $promise, \JSON_THROW_ON_ERROR);
+            $value = yield $promise;
+
+            if (\PHP_VERSION >= 70300) {
+                $result = json_encode($value, \JSON_THROW_ON_ERROR);
+            } else {
+                $result = json_encode($value);
+
+                if (($lastError = json_last_error()) !== \JSON_ERROR_NONE) {
+                    throw new \JsonException(json_last_error_msg(), $lastError);
+                }
+            }
 
             $response = new Response;
             $response->setHeader('content-type', 'application/json');
