ee090a5802c03b71208d887ed4e3409db355b1cd
Jakub Groncki <jakobmats@dahmer.home>
2019-09-21 20:12:54 +0200

    Added PlainTextTransformer

---
+++ src/PlainTextTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Empress;
+
+use Amp\Http\Server\Response;
+use Amp\Promise;
+use Amp\Success;
+
+use function Amp\call;
+
+class PlainTextTransformer implements ResponseTransformerInterface
+{
+
+    /** @inheritDoc */
+    public function transform(Promise $promise): Promise
+    {
+        return call(function () use ($promise) {
+            $value = yield $promise;
+
+            if (!\is_string($value)) {
+                throw new \TypeError('Expected string');
+            }
+
+            $response = new Response();
+            $response->setHeader('content-type', 'text/plain');
+            $response->setBody($value);
+
+            return new Success($response);
+        });
+    }
+}
