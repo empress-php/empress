6bb3bdefb61facb63eb683ce4f86162d35fbe8aa
Jakub Groncki <jakobmats@dahmer.home>
2019-09-21 20:13:19 +0200

    Updated tests

--- test/Routing/RouteDefinitionTest.php
+++ test/Routing/RouteDefinitionTest.php
@@ -4,17 +4,13 @@ namespace Empress\Test\Routing;
 
 use Empress\Routing\RouteDefinition;
 use PHPUnit\Framework\TestCase;
-use function Empress\Routing\route;
 
 class RouteDefinitionTest extends TestCase
 {
-    public function testDefinitionsAreEquivalent()
+    public function testVerbIsCapitalized()
     {
-        $shorthandDefinition = route('POST', '/', 'someHandler');
-        $definition = new RouteDefinition('POST', '/', 'someHandler');
+        $route = new RouteDefinition('post', '/', 'someHandler');
 
-        $this->assertEquals($definition->getVerb(), $shorthandDefinition->getVerb());
-        $this->assertEquals($definition->getUri(), $shorthandDefinition->getUri());
-        $this->assertEquals($definition->getHandler(), $shorthandDefinition->getHandler());
+        $this->assertEquals('POST', $route->getVerb());
     }
 }
