375e7422e6d42733f280eeb95d481beb6e9308b2
Jakub Groncki <jakub.groncki@gmail.com>
2019-09-24 23:54:26 +0200

    Added DefaultHeadersMiddleware

---
+++ src/Middleware/DefaultHeadersMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Empress\Middleware;
+
+use Amp\Http\Server\Middleware;
+use Amp\Http\Server\Request;
+use Amp\Http\Server\RequestHandler;
+use Amp\Promise;
+
+use function Amp\call;
+
+/**
+ * Middleware that adds additional response headers to each response.
+ */
+class DefaultHeadersMiddleware implements Middleware
+{
+
+    /** @var array */
+    private $headers;
+
+    /**
+     * @param array $headers Default headers to be used with every response
+     */
+    public function __construct(array $headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /** @inheritDoc */
+    public function handleRequest(Request $request, RequestHandler $requestHandler): Promise
+    {
+        return call(function () use ($request, $requestHandler) {
+
+            /** @var \Amp\Http\Server\Response $response */
+            $response = yield $requestHandler->handleRequest($request);
+            $response->setHeaders($this->headers);
+            \dump($response->getHeaders());
+
+            return $response;
+        });
+    }
+}
