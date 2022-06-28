95b287ab6817e25d054bf221506a0f2c7c69f513
Jakub Tapuć <jakub.groncki@gmail.com>
2022-06-28 20:25:19 +0200

    New error handler

---
+++ src/DefaultErrorHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Empress;
+
+use Amp\Http\Server\ErrorHandler;
+use Amp\Http\Server\Request;
+use Amp\Http\Server\Response;
+use Amp\Promise;
+use Amp\Success;
+
+final class DefaultErrorHandler implements ErrorHandler
+{
+    public function __construct()
+    {
+    }
+
+    public function handleError(int $statusCode, ?string $reason = null, ?Request $request = null): Promise
+    {
+        static $errorHtml;
+
+        if ($errorHtml === null) {
+            $errorHtml = \file_get_contents(__DIR__ . '/../static/error.html');
+        }
+
+        $responseBody = \str_replace(
+            ['{{error_code}}', '{{error_trace}}'],
+            [$statusCode, $reason === null ? '' : \sprintf('<pre>%s</pre>', $reason)],
+            $errorHtml
+        );
+
+        $response = new Response($statusCode, [
+            "content-type" => "text/html; charset=utf-8",
+        ], $responseBody);
+
+        $response->setStatus($statusCode, $reason);
+
+        return new Success($response);
+    }
+}
