693f30c719d3f6e4308a2cccf1fc8ef7f34f4abd
Jakub Tapuć <jakub.groncki@gmail.com>
2022-06-29 16:14:02 +0200

    Cache preg_match results

--- src/Routing/Path/PathMatcher.php
+++ src/Routing/Path/PathMatcher.php
@@ -4,34 +4,69 @@ declare(strict_types=1);
 
 namespace Empress\Routing\Path;
 
+use cash\LRUCache;
+
 final class PathMatcher
 {
+    private const CACHE_SIZE = 512;
+
+    private LRUCache $cache;
+
     public function __construct(private string $regex)
     {
+        $this->cache = new LRUCache(self::CACHE_SIZE);
     }
 
     public function matches(string $toMatch): bool
     {
-        return \preg_match($this->regex, $toMatch) === 1;
+        if ($this->cache->containsKey($toMatch)) {
+            return $this->cache->get($toMatch);
+        }
+
+        $result = \preg_match($this->regex, $toMatch) === 1;
+
+        $this->cache->put($toMatch, $result);
+
+        return $result;
     }
 
     public function extractNamedParams(string $toMatch): array
     {
+        $cacheKey = $toMatch . '_PARAMS';
+
+        if ($this->cache->containsKey($cacheKey)) {
+            return $this->cache->get($cacheKey);
+        }
+
         \preg_match($this->regex, $toMatch, $matches);
 
-        return \array_filter($matches, fn (mixed $key) => \is_string($key), \ARRAY_FILTER_USE_KEY);
+        $params = \array_filter($matches, fn (mixed $key) => \is_string($key), \ARRAY_FILTER_USE_KEY);
+
+        $this->cache->put($cacheKey, $params);
+
+        return $params;
     }
 
     public function extractWildcards(string $toMatch): array
     {
+        $cacheKey = $toMatch . '_WILDCARDS';
+
+        if ($this->cache->containsKey($cacheKey)) {
+            return $this->cache->get($cacheKey);
+        }
+
         \preg_match($this->regex, $toMatch, $matches);
 
-        return \array_values(
+        $wildcards = \array_values(
             \array_filter(
                 \array_splice($matches, 1),
                 fn (mixed $key) => \is_int($key),
                 \ARRAY_FILTER_USE_KEY
             )
         );
+
+        $this->cache->put($cacheKey, $wildcards);
+
+        return $wildcards;
     }
 }
