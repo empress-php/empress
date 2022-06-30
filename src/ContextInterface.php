8d18b8c4fff4316698bec0859f56e99a4c28a92f
Jakub Tapuć <jakub.groncki@gmail.com>
2022-06-30 17:23:07 +0200

    ecs

--- src/ContextInterface.php
+++ src/ContextInterface.php
@@ -4,24 +4,11 @@ declare(strict_types=1);
 
 namespace Empress;
 
-use Amp\ByteStream\InputStream;
-use Amp\Http\Cookie\CookieAttributes;
-use Amp\Http\Cookie\InvalidCookieException;
-use Amp\Http\Cookie\RequestCookie;
-use Amp\Http\Server\FormParser\Form;
 use Amp\Http\Server\Request;
-use Amp\Http\Server\RequestBody;
 use Amp\Http\Server\Response;
-use Amp\Http\Server\Session\Session;
-use Amp\Http\Status;
-use Amp\Iterator;
-use Amp\Promise;
-use Empress\Routing\HaltException;
-use Empress\Validation\ValidationContext;
 
 interface ContextInterface
 {
-
     /**
      * Gets the underlying request object.
      */
